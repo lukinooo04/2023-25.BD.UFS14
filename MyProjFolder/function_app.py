3d6dd693b5f85039b1ddf448cf2a4c315ef3df4e
lukinooo04 <luca.asara@itsrizzoli.it>
2024-11-06 14:03:56 +0000

    provamagaricorretta

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -2,35 +2,39 @@ import azure.functions as func
 import datetime
 import json
 import logging
+import re  # Import the re module for regular expressions
 
 app = func.FunctionApp()
+
+def highlight_numbers(text):
+    text = re.sub(r'(\d+,\d+\.?\d*)', r'<b style="color:red;">\1</b>', text)
+    
+    highlight_words = ["Ciao", "Bello", "Aurora", "Amo", "William"]
+    if highlight_words:
+        pattern = r'\b(' + '|'.join(re.escape(word) for word in highlight_words) + r')\b'
+        text = re.sub(pattern, r'<span style="color:yellow">\1</span>', text)
+    
+    return text
 
 @app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    cognome = req.params.get('cognome')
+    text = req.params.get('text')
 
-    if not name:
+    if not text:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            text = req_body.get('text')
 
-    if name:
-        if cognome:
-            return func.HttpResponse(f"Hello, {name} {cognome}. This HTTP triggered function executed successfully.")
-        else:
-            return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if text:
+        highlighted_text = highlight_numbers(text)
+        return func.HttpResponse(f"Processed text: {highlighted_text}", mimetype="text/html")
     else:
         return func.HttpResponse(
-             "AHAHAH.",
-             status_code=200
+             "No text provided.",
+             status_code=400
         )
-
-
-
-#ciao
